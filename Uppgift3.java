ecd13829b121866fbfb655831a4726766e0462a3
Robin-Malmstrom <robin.malmstrom@elev.ga.ntig.se>
2019-03-18 13:57:11 +0100

    Initial commit

---
+++ Uppgift3.java
@@ -0,0 +1,16 @@
+package uppgift3;
+
+import javax.swing.JOptionPane;
+
+//Uppgift 8.3, sida 174, endast stubbe
+
+public class Uppgift3 {
+
+	public static void main(String[] args) {
+		
+	}
+	
+	public static boolean ärEttPrimtal(int tal) {
+		return false;
+	}
+}
